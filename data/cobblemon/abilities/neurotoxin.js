b2d8b5477f3a872dee2fbed579dd40f3f3ef2937
Saucecoie <bladex406@gmail.com>
2026-03-01 17:02:37 -0500

    neurotoxin implemented

---
+++ data/cobblemon/abilities/neurotoxin.js
@@ -0,0 +1,11 @@
+{
+		onSetStatus(status, target, source, effect) {
+			if (status.id !== 'psn' && status.id !== 'tox') return;
+			this.add('-ability', source, 'Neurotoxin');
+			this.boost({ spe: -1 }, target, source, null, true);
+			return true;
+		},
+		flags: { breakable: 1 },
+		name: "Brain Worm",
+		rating: 3,
+	}
