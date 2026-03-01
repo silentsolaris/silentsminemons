ae2510d91697d87c78bb5953495a8bad17dbf8bb
Saucecoie <bladex406@gmail.com>
2026-03-01 15:37:37 -0500

    soulfire blade implemented

---
+++ data/cobblemon/moves/soufireblade.js
@@ -0,0 +1,17 @@
+{
+	num: 42000,
+    	accuracy: 100,
+		basePower: 80,
+		category: "Physical",
+		name: "Soulfire Blade",
+		pp: 10,
+		priority: 0,
+		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1, slicing: 1 },
+		secondary: {
+			chance: 30,
+			status: 'brn',
+		},
+		target: "normal",
+		type: "Poison",
+		contestType: "Cool",
+}
