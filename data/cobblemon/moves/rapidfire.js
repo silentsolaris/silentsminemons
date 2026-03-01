26a0592a72d318b774ec085d2f127016dd171b17
Saucecoie <bladex406@gmail.com>
2026-03-01 15:59:35 -0500

    rapid fire implemented

---
+++ data/cobblemon/moves/rapidfire.js
@@ -0,0 +1,23 @@
+{
+	num: 42002,
+    	accuracy: 100,
+		basePower: 25,
+		category: "Physical",
+		name: "Rapid Fire",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1, bullet: 1 },
+		critRatio: 2,
+		secondaries: [
+			{
+				chance: 10,
+				status: 'par'
+			}, {
+				chance: 10,
+				volatileStatus: 'flinch',
+			},
+		],
+		target: "normal",
+		type: "Ground",
+		contestType: "Tough",
+}
