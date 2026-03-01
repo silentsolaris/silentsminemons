44342b1af6c4f86a3efd7142782e0f2f1caf8bce
Saucecoie <bladex406@gmail.com>
2026-03-01 16:31:53 -0500

    fixed sink teeth probably

--- data/cobblemon/moves/sinkteeth.js
+++ data/cobblemon/moves/sinkteeth.js
@@ -6,8 +6,12 @@
 		name: "Sink Teeth",
 		pp: 15,
 		priority: 0,
-		flags: { contact: 1, protect: 1, mirror: 1, heal: 1, metronome: 1, bite: 1},
-		heal: [1, 4],
+		flags: { contact: 1, protect: 1, mirror: 1, heal: 1, metronome: 1, bite: 1}
+		self: {
+			onHit(pokemon) {
+				this.heal(pokemon.baseMaxhp / 4);
+			},
+		},
 		target: "normal",
 		type: "Ghost",
 		contestType: "Tough",
