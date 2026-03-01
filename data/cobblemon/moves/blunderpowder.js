4e89b71291db241585f2d65fbc4032f0b96b7677
Saucecoie <bladex406@gmail.com>
2026-03-01 16:28:18 -0500

    fixed rapid fire multihit

--- data/cobblemon/moves/blunderpowder.js
+++ data/cobblemon/moves/blunderpowder.js
@@ -13,7 +13,7 @@
 		},
 		onHit(target, source, move) {
 			for (const ally of target.adjacentAllies()) {
-				this.add(ally.baseSpecies);
+				this.add(ally);
 				this.damage(ally.baseMaxhp / 16, ally, source, this.dex.conditions.get('Flame Burst'));
 				if (this.randomChance(3, 10)) {
 					source.trySetStatus('tox', ally);
@@ -22,7 +22,7 @@
 		},
 		onAfterSubDamage(damage, target, source, move) {
 			for (const ally of target.adjacentAllies()) {
-				this.add(ally.baseSpecies);
+				this.add(ally);
 				this.damage(ally.baseMaxhp / 16, ally, source, this.dex.conditions.get('Flame Burst'));
 				if (this.randomChance(3, 10)) {
 					source.trySetStatus('tox', ally);
--- data/cobblemon/moves/rapidfire.js
+++ data/cobblemon/moves/rapidfire.js
@@ -7,6 +7,7 @@
 		pp: 10,
 		priority: 0,
 		flags: { protect: 1, mirror: 1, metronome: 1, bullet: 1 },
+		multihit: 3,
 		critRatio: 2,
 		secondaries: [
 			{
