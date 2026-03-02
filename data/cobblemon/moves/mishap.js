37ca4b520334afc1fa2d1e757e6a22a4cdae0597
Saucecoie <bladex406@gmail.com>
2026-03-02 17:05:57 -0500

    mishap

---
+++ data/cobblemon/moves/mishap.js
@@ -0,0 +1,19 @@
+{
+	num: 42005,
+		accuracy: 85,
+		basePower: 0,
+		category: "Status",
+		name: "Mishap",
+		pp: 15,
+		priority: 0,
+		flags: { protect: 1, reflectable: 1, mirror: 1, allyanim: 1, metronome: 1 },
+		volatileStatus: 'confusion',
+		boosts: {
+			spe: 2,
+		},
+		secondary: null,
+		target: "normal",
+		type: "Poison",
+		zMove: { effect: 'clearnegativeboost' },
+		contestType: "Clever",
+}
