44db3d67b1bfa844b880fa9e60c6ad9340b72f8a
Saucecoie <bladex406@gmail.com>
2026-03-02 18:37:04 -0500

    MAYBE implemented dealer

---
+++ data/cobblemon/moves/dealer.js
@@ -0,0 +1,31 @@
+{
+	num: 42006,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		name: "Dealer",
+		pp: 10,
+		priority: 0,
+		flags: { bypasssub: 1 },
+		onHitField(target, source, move) {
+			const targets = this.getAllActive();
+			var items = [];
+			for (const pokemon of targets) {
+				if (target.item) {
+					items.push(pokemon.item);
+					this.add("Added " + pokemon.item + " to list.");
+				}
+			}
+			for (const item of items) {
+				let pkmn = this.sample(targets);
+				const index = targets.indexOf(pkmn);
+				pkmn.item = item;
+				targets.splice(index, 1);
+				this.add("The held items on the field were shuffled!");
+				this.add(pkmn.baseSpecies.name + " obtained the " + item + "!");
+			}
+		},
+		target: "all",
+		type: "Normal",
+		contestType: "Clever",
+	}
