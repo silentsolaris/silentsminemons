0ea9751377f62f9afa00a3358015a5adfa3a4b38
Saucecoie <bladex406@gmail.com>
2026-03-02 17:23:45 -0500

    savagery ability implemented

---
+++ data/cobblemon/abilities/savagery.js
@@ -0,0 +1,28 @@
+{
+		onUpdate(pokemon) {
+			if (pokemon.volatiles['confusion']) {
+				this.add('-activate', pokemon, 'ability: Savagery');
+				pokemon.removeVolatile('confusion');
+			}
+		},
+		onTryAddVolatile(status, pokemon) {
+			if (status.id === 'confusion'){
+				let stats = [];
+     			const boost = {};
+				let statPlus;
+				for (statPlus in pokemon.boosts) {
+					if (statPlus === "accuracy" || statPlus === "evasion")
+					continue;
+					if (pokemon.boosts[statPlus] < 6) {
+						stats.push(statPlus);
+					}
+				}
+				let randomStat = stats.length ? this.sample(stats) : void 0;
+				if (randomStat)
+				boost[randomStat] = 1;
+			}
+		},
+		flags: {},
+		name: "Savagery",
+		rating: 3,
+	}
