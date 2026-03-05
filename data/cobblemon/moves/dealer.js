c23f7a9d11ce4fa4b0955af754ba2aea62fc7685
Saucecoie <bladex406@gmail.com>
2026-03-05 18:39:22 -0500

    fixed dealer

--- data/cobblemon/moves/dealer.js
+++ data/cobblemon/moves/dealer.js
@@ -10,17 +10,15 @@
 		onHitField(target, source, move) {
 			const targets = this.getAllActive();
 			var items = [];
-			for (const pokemon of targets) {
-				if (target.item) {
-					items.push(pokemon.item);
-					this.add("Added " + pokemon.item + " to list.");
-				}
+			for (const pkmn of targets) {
+				if (!pkmn.item) continue;
+				items.push(pkmn.item);
 			}
 			this.add("The held items on the field were shuffled!");
 			for (const item of items) {
 				let pkmn = this.sample(targets);
 				const index = targets.indexOf(pkmn);
-				pokemon.setItem(item, source, move);
+				pkmn.setItem(item, source, move);
 				targets.splice(index, 1);
 				this.add(pkmn.baseSpecies.name + " obtained the " + item + "!");
 			}
