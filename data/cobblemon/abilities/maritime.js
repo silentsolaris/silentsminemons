b1d530276d1cd18262fdd18182daa48c7fcd9a02
Saucecoie <bladex406@gmail.com>
2026-03-05 21:08:22 -0500

    maritime works now

--- data/cobblemon/abilities/maritime.js
+++ data/cobblemon/abilities/maritime.js
@@ -1,13 +1,17 @@
 {
 		onBasePower(basePower, pokemon, target, move) {
-			if (['raindance', 'primordialsea'].includes(pokemon.effectiveWeather() && move.type === 'rock')) {
-				return this.chainModify(1.5);
+			if (['raindance', 'primordialsea'].includes(pokemon.effectiveWeather())) {
+				if (move.type === "Rock") {
+					return this.chainModify(1.5);
+				}
 			}
 		},
 		onSourceModifyAccuracyPriority: -1,
 		onSourceModifyAccuracy(accuracy, target, source, move) {
-			if (['raindance', 'primordialsea'].includes(pokemon.effectiveWeather() && move.type === 'rock')) {
-				return true;
+			if (['raindance', 'primordialsea'].includes(source.effectiveWeather())) {
+				if (move.type === "Rock") {
+					return true;
+				}
 			}
 		},
 		flags: {},
