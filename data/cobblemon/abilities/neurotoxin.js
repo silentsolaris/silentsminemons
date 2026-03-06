15df0a0b07640e9fda74523aef9a08757aeb4551
Saucecoie <bladex406@gmail.com>
2026-03-05 19:53:01 -0500

    neurotoxin works

--- data/cobblemon/abilities/neurotoxin.js
+++ data/cobblemon/abilities/neurotoxin.js
@@ -1,10 +1,12 @@
 {
-		onSetStatus(status, target, source, effect) {
-			if (status.id !== 'psn' && status.id !== 'tox') return;
-			this.add('-ability', source, 'Neurotoxin');
-			this.boost({ spe: -1 }, target, source, null, true);
-			return true;
-		},
+	onAnyAfterSetStatus(status, target, source, effect) {
+      if (target === source || target.isAlly(this.effectState.target))
+        return;
+      if (status.id === "psn" || status.id === "tox") {
+		this.add('-ability', source, 'Neurotoxin');
+		this.boost({ spe: -1 }, target, source, null, true);
+      }
+    },
 		flags: { breakable: 1 },
 		name: "Neurotoxin",
 		rating: 3,
