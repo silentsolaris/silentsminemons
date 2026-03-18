7174b747ce8e5ed094e5b1007c73d8842d477596
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-18 12:38:44 -0400

    enjoy dark sorcery

---
+++ data/cobblemon/abilities/darksorcery.js
@@ -0,0 +1,12 @@
+{
+		onModifyMovePriority: -5,
+		onModifyMove(move) {
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity['Psychic'] = true;
+			}
+		},
+		flags: {},
+		name: "Dark Sorcery",
+		rating: 3,
+	}
