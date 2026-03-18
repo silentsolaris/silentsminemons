9dbffed03230d49567da5295f5422ebcc02fa8d2
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-17 22:47:02 -0400

    enjoy determination

---
+++ data/cobblemon/abilities/determination.js
@@ -0,0 +1,19 @@
+{
+		onStart(pokemon) {
+			pokemon.abilityState.undying = false;
+		},
+		onDamage(damage, target, source, effect) {
+			if (damage >= target.hp && effect && effect.effectType === 'Move') {
+				this.add("-activate", target, "ability: Determination");
+				target.abilityState.undying = true;
+				return target.hp - 1;
+			}
+		},
+		onResidual(pokemon) {
+			if (pokemon.abilityState.undying === false) return;
+			pokemon.faint();
+		},
+		flags: {},
+		name: "Determination",
+		rating: 3,
+}
