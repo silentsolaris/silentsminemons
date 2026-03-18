d9b2f4769858b87c9d2d7b0df807631d811abbed
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-17 22:37:09 -0400

    "enjoy" blastospore

---
+++ data/cobblemon/moves/blastospore.js
@@ -0,0 +1,14 @@
+{
+	num: 42001,
+    	accuracy: 100,
+		basePower: 250,
+		category: "Physical",
+		name: "Blastospore",
+		pp: 5,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1, noparentalbond: 1, powder: 1 },
+		selfdestruct: "always",
+		target: "allAdjacentFoes",
+		type: "Grass",
+		contestType: "Tough",
+}
