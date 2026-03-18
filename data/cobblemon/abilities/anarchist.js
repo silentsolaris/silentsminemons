d7735cb8d579df85f034126eb6d328dd91a13e45
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-18 12:52:38 -0400

    enjoy anarchist

--- data/cobblemon/abilities/anarchist.js
+++ data/cobblemon/abilities/anarchist.js
@@ -1,21 +1,27 @@
 {
-		onSourceModifyAtkPriority: 6,
-		onSourceModifyAtk(atk, attacker, defender, move) {
-			if (move.type === 'Fighting') {
-				this.debug('Anarchist weaken');
-				return this.chainModify(0.5);
-			}
-		},
-		onSourceModifySpAPriority: 5,
-		onSourceModifySpA(atk, attacker, defender, move) {
-			if (move.type === 'Fighting') {
-				this.debug('Anarchist weaken');
-				return this.chainModify(0.5);
-			}
-		},
 		onDamagingHit(damage, target, source, move) {
 			if (move.type === 'Fighting') {
 				this.boost({ atk: 1 });
+			}
+		},
+		onUpdate(pokemon) {
+			if (pokemon.volatiles['confusion']) {
+				this.add('-activate', pokemon, 'ability: Anarchist');
+				pokemon.removeVolatile('confusion');
+			}
+		},
+		onTryAddVolatile(status, pokemon) {
+			if (status.id === 'confusion') return null;
+		},
+		onHit(target, source, move) {
+			if (move?.volatileStatus === 'confusion') {
+				this.add('-immune', target, 'confusion', '[from] ability: Anarchist');
+			}
+		},
+		onTryBoost(boost, target, source, effect) {
+			if (effect.name === 'Intimidate' && boost.atk) {
+				delete boost.atk;
+				this.add('-fail', target, 'unboost', 'Attack', '[from] ability: Anarchist', `[of] ${target}`);
 			}
 		},
 		flags: { breakable: 1},
