04dec8e4f24511d9d4721432a318d7fac1fecc24
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-19 08:09:03 -0400

    anomalous and bad input now work as expected

--- data/cobblemon/abilities/anomalous.js
+++ data/cobblemon/abilities/anomalous.js
@@ -1,52 +1,12 @@
 {
-		
-			onResidualOrder: 28,
-			onResidualSubOrder: 2,
-			onResidual(pokemon) {
-				let typelist = [
-					"Normal",
-					"Fire",
-					"Water",
-					"Electric",
-					"Grass",
-					"Ice",
-					"Fighting",
-					"Poison",
-					"Ground",
-					"Flying",
-					"Psychic",
-					"Bug",
-					"Rock",
-					"Ghost",
-					"Dragon",
-					"Dark",
-					"Steel",
-					"Fairy"
-				];
-
-				let newtype = this.sample(typelist);
-				let newtypes = [newtype];
-				const index = typelist.indexOf(newtype);
-				typelist.splice(index, 1);
-				if ((this.randomChance(1, 18))) {
-					newtypes.push(this.sample(typelist));
-					this.add(pokemon + "'s types changed to " + newtypes[0], newtypes[1]);
-					if (!pokemon.setType(newtypes)) return false;
-					return;
-				}
-				
-				this.add(pokemon + "'s type changed to " + newtypes[0]);
-				if (!pokemon.setType(newtypes)) return false;
-			},
-			onEffectivenessPriority: -2,
-			onEffectiveness(typeMod, target, type, move) {
-				if (move.category === 'Status') return;
-				if (target.runImmunity(move)) {
-					return 1;
-				}
-				return typeMod * -1;
-			},
-			
+		onEffectivenessPriority: -2,
+		onEffectiveness(typeMod, target, type, move) {
+			if (move.category === 'Status') return;
+			if (target.runImmunity(move)) {
+				return 1;
+			}
+			return typeMod * -1;
+		},
 		flags: {},
 		name: "Anomalous",
 		rating: 3,
---
+++ data/cobblemon/moves/badinput.js
@@ -0,0 +1,51 @@
+{
+	num: 42001,
+    	accuracy: 95,
+		basePower: 100,
+		category: "Physical",
+		name: "Bad Input",
+		pp: 5,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1},
+		onModifyMove(move, pokemon) {
+			if (pokemon.getStat('atk', false, true) < pokemon.getStat('spa', false, true)) move.category = 'Special';
+		},
+		onAfterMoveSecondary(target, source, move) {
+			let typelist = [
+					"Normal",
+					"Fire",
+					"Water",
+					"Electric",
+					"Grass",
+					"Ice",
+					"Fighting",
+					"Poison",
+					"Ground",
+					"Flying",
+					"Psychic",
+					"Bug",
+					"Rock",
+					"Ghost",
+					"Dragon",
+					"Dark",
+					"Steel",
+					"Fairy"
+				];
+
+				let newtype = this.sample(typelist);
+				let newtypes = [newtype];
+				const index = typelist.indexOf(newtype);
+				typelist.splice(index, 1);
+				if ((this.randomChance(1, 18))) {
+					this.add(source + "'s type changed to " + newtypes[0]);
+					if (!source.setType(newtypes)) return false;
+					return;
+				}
+				newtypes.push(this.sample(typelist));
+				this.add(source + "'s types changed to " + newtypes[0], newtypes[1]);
+				if (!source.setType(newtypes)) return false;
+		},
+		target: "normal",
+		type: "Normal",
+		contestType: "Clever",
+}
