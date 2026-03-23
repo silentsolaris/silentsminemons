0c22dbdd5c40b4831be5aadad02ee9c2e055c019
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-23 15:51:54 -0400

    traces for debugging on eldereye

--- data/cobblemon/moves/eldereye.js
+++ data/cobblemon/moves/eldereye.js
@@ -8,6 +8,7 @@
 		priority: 1,
 		flags: { protect: 1, mirror: 1, metronome: 1, pulse: 1 },
 		onHit(target, source, move) {
+			this.add(source.lastMoveUsed + " is " + source.lastMoveUsed.type + " type.");
 			if (source.lastMoveUsed.type !== 'Electric') return;
 			for (const pokemon of this.getAllActive()) {
 				if (pokemon === source) continue;
