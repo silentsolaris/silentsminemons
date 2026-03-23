8c9761172603240bc3e051fbba7ed1d8881fc01a
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-23 17:40:17 -0400

    ADDED EVEN MROE TRAZCES FOR APPETITE

--- data/cobblemon/abilities/appetite.js
+++ data/cobblemon/abilities/appetite.js
@@ -10,7 +10,9 @@
 				this.add(pokemon.baseSpecies);
 				this.add(pokemon.baseSpecies.forme);
 				if (pokemon.baseSpecies !== 'gourmooze' || pokemon.transformed || !pokemon.hp) return;
+				this.add("Ok we got a gourmooze ova here!");
 				if (pokemon.baseSpecies.forme === 'Obese') return;
+				this.add("And he's not obese!!!");
 				this.add('-activate', pokemon, 'ability: Appetite');
 				pokemon.formeChange('gourmooze-Obese', this.effect, true);
 				pokemon.formeRegression = true;
