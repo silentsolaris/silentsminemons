384b589bbf056debb9e1312ee73645f7015d2c1d
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-23 17:10:44 -0400

    fixed msg on brain worm

--- data/cobblemon/abilities/brainworm.js
+++ data/cobblemon/abilities/brainworm.js
@@ -3,7 +3,7 @@
 			if (this.checkMoveMakesContact(move, source, target)) {
 				if (source.hasType('Psychic')) return false;
 				if (!source.addType('Psychic')) return false;
-				this.add('-start', target, 'typeadd', 'Psychic', '[from] ability: Brain Worm');
+				this.add('-start', source, 'typeadd', 'Psychic', '[from] ability: Brain Worm');
 			}
 		},		
 		flags: { breakable: 1 },
