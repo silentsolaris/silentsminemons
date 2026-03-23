1133b1aee3c57fbb5781067bf73a36ba5409faeb
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-23 15:58:47 -0400

    fixed elder eye checking for last move used too late in the sim

--- data/cobblemon/moves/eldereye.js
+++ data/cobblemon/moves/eldereye.js
@@ -7,9 +7,12 @@
 		pp: 5,
 		priority: 1,
 		flags: { protect: 1, mirror: 1, metronome: 1, pulse: 1 },
+		onStart(target, source, effect) {
+				this.effectState.lastMoveElectricType = source.lastMoveUsed.type === 'Electric';
+				this.add(source.lastMoveUsed + " was " + source.lastMoveUsed.type + " type.");
+			},
 		onHit(target, source, move) {
-			this.add(source.lastMoveUsed + " is " + source.lastMoveUsed.type + " type.");
-			if (source.lastMoveUsed.type !== 'Electric') return;
+			if (this.effectState.lastMoveElectricType) return;
 			for (const pokemon of this.getAllActive()) {
 				if (pokemon === source) continue;
 				pokemon.addVolatile('electrify');
