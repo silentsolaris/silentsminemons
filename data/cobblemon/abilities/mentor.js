64891fd5ad673cfbe8ef506ec8b5e03b9f092313
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-24 11:46:21 -0400

    mentor test 4

--- data/cobblemon/abilities/mentor.js
+++ data/cobblemon/abilities/mentor.js
@@ -3,8 +3,8 @@
 			if (!pokemon.side || pokemon.side.active.length < 2) return spe;
 
 			const ally = pokemon.allies().find(p => p && !p.fainted);
-			this.add(ally.baseSpecies);
 			if (!ally) return spe;
+			this.add(ally.baseSpecies);
 
 			this.add('-activate', pokemon, 'ability: Mentor');
 
