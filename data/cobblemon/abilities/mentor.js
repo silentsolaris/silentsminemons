c9a08f9246960285ac07599ff992cb9be9e50360
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-03-24 11:38:24 -0400

    mentor test 1

--- data/cobblemon/abilities/mentor.js
+++ data/cobblemon/abilities/mentor.js
@@ -11,29 +11,6 @@
 			if (!myAction || myAction.choice !== 'move') return spe;
 			if (!allyAction || allyAction.choice !== 'move') return spe;
 
-			const myPriority = this.runEvent(
-				'ModifyPriority',
-				pokemon,
-				myAction.target,
-				myAction.move,
-				myAction.move.priority
-			);
-
-			const allyPriority = this.runEvent(
-				'ModifyPriority',
-				ally,
-				allyAction.target,
-				allyAction.move,
-				allyAction.move.priority
-			);
-
-			if (myPriority !== allyPriority) return spe;
-
-			const mySpeed = pokemon.getActionSpeed();
-			const allySpeed = ally.getActionSpeed();
-
-			if (mySpeed >= allySpeed) return spe;
-
 			this.add('-activate', pokemon, 'ability: Mentor');
 
 			return allySpeed - 1;
