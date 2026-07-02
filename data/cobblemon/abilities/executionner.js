0cb91326730778ddc29790f44582de1a8c37f362
Silent <108959595+Silentseekers14@users.noreply.github.com>
2026-07-02 19:41:42 -0400

    implementation
    - Reworked Heroism & Executionner abilities
    - Fully Implemented Snowbrawl
    - Some poser tweaks

--- data/cobblemon/abilities/executionner.js
+++ data/cobblemon/abilities/executionner.js
@@ -1,22 +1,13 @@
 {
-        onBasePowerPriority: 19,
 		onBasePower(basePower, attacker, defender, move) {
-			if (move.flags['slicing']) {
-				this.debug('Executionner boost');
-				return this.chainModify(1.5);
+			if (target.maxhp = target.hp) {
+				this.debug('Executionner Normal');
+				return this.chainModify(0.5);
+
 			}
-		},
-        onTryHit(pokemon, target, move) {
-			if (move.ohko) {
-				this.add('-immune', pokemon, '[from] ability: Executionner');
-				return null;
-			}
-		},
-		onDamagePriority: -30,
-		onDamage(damage, target, source, effect) {
-			if (target.hp === target.maxhp && damage >= target.hp && effect && effect.effectType === 'Move') {
-				this.add('-ability', target, 'Executionner');
-				return target.hp - 1;
+			else {
+				this.debug('Executionner Boost');
+				return chainmodify(1.2 + (target.hp / target.maxhp))
 			}
 		},
 		flags: {},
--- data/cobblemon/abilities/heroism.js
+++ data/cobblemon/abilities/heroism.js
@@ -1,28 +1,16 @@
 {
-    onModifyAtkPriority: 5,
-		onModifyAtk(atk, attacker, defender, move) {
-			if (move.type === 'Fighting') {
-				this.debug('Heroism boost');
-				return this.chainModify(1.5);
+		onBasePower(basePower, attacker, defender, move) {
+			if (pokemon.maxhp = pokemon.hp) {
+				this.debug('Heroism Normal');
+				return this.chainModify(0.8);
+
+			}
+			else {
+				this.debug('Heroism Boost');
+				return chainmodify(1.2 + ( pokemon.hp / pokemon.maxhp))
 			}
 		},
-		onModifySpAPriority: 5,
-		onModifySpA(atk, attacker, defender, move) {
-			if (move.type === 'Fighting') {
-				this.debug('Heroism boost');
-				return this.chainModify(1.5);
-			}
-		},
-        onTryAddVolatile(status, pokemon) {
-			if (status.id === 'flinch') return null;
-		},
-		onTryBoost(boost, target, source, effect) {
-			if (effect.name === 'Intimidate' && boost.atk) {
-				delete boost.atk;
-				this.add('-fail', target, 'unboost', 'Attack', '[from] ability:  Heroism', `[of] ${target}`);
-			}
-		},
-		flags: {},
-		name: "Heroism",
-		rating: 3.5,
+			flags: {},
+			name: "Heroism",
+			rating: 5,
 }
