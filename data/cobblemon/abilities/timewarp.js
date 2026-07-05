d321771fdfd42820023b8b053d401fe2c6a67830
Silent <108959595+Silentseekers14@users.noreply.github.com>
2026-07-05 15:44:55 -0400

    tweaks and fixes
    I'm so fucking pissed rn

--- data/cobblemon/abilities/timewarp.js
+++ data/cobblemon/abilities/timewarp.js
@@ -1,12 +1,12 @@
 {
     onStart(pokemon) {
 		let activated = false;
-			if this.field.getPseudoWeather('trickroom') {
+			if (this.field.getPseudoWeather('trickroom')) {
 				this.boost({ spe: -2 }, pokemon);
 				this.field.setPseudoWeather('trickroom');
 				this.field.setPseudoWeather('trickroom');
-			},
-			activated = true;
+				activated = true;
+			}
 		},
 		flags: {},
 		name: "Time Warp",
