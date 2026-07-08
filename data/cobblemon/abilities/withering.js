d81f8ec86c076d95d43b835aa26cd452dd7ae214
Silent <108959595+Silentseekers14@users.noreply.github.com>
2026-07-08 11:08:40 -0400

    Woolipup color dyeing/shearing ACTIVATE!!!
    - oh yeah, added a signature ability for Withereign that allows poison moves to hit steel types cuz corrosion wasn't doing it for me

---
+++ data/cobblemon/abilities/withering.js
@@ -0,0 +1,12 @@
+{
+		onModifyMovePriority: -5,
+		onModifyMove(move) {
+			if (!move.ignoreImmunity) move.ignoreImmunity = {};
+			if (move.ignoreImmunity !== true) {
+				move.ignoreImmunity['Poison'] = true;
+			}
+		},
+		flags: {},
+		name: "Withering",
+		rating: 3,
+	}
