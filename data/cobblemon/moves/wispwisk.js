a565d90f5e762d57dbde3fdd1030b6f716be13e7
Silent <108959595+Silentseekers14@users.noreply.github.com>
2026-09-24 10:56:17 -0400

    Musheteer Buff
    Made Wisp Wisk, a ghost type bullet move to combo with Musheteer's Mortar ability.

---
+++ data/cobblemon/moves/wispwisk.js
@@ -0,0 +1,19 @@
+{
+	num: 42050,
+    	accuracy: 100,
+		basePower: 80,
+		category: "Physical",
+		name: "Wisp Wisk",
+		pp: 10,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1, bullet: 1 },
+		secondary: {
+			chance: 30,
+			boosts: {
+				atk: -1,
+			},
+		},
+		target: "normal",
+		type: "Ghost",
+		contestType: "Cool",
+}
